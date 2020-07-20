fb13f5bb6ef70dfed3709b4f581ac0ae0f5290c7
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-07-20 19:39:36 +0300

    First build

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+# node-addon-api Conan package
+# Dmitriy Vetutnev, Odant, 2020
+
+
+from conans import ConanFile
+
+
+class CImgConan(ConanFile):
+    name = "node-addon-api"
+    version = "3.0.1"
+    license = "MIT https://raw.githubusercontent.com/nodejs/node-addon-api/master/LICENSE.md"
+    description = "This module contains header-only C++ wrapper classes which simplify the use of the C based N-API provided by Node.js when using C++. It provides a C++ object model and exception handling semantics with low overhead."
+    url = "https://github.com/odant/conan-node-addon-api"
+    exports_sources = "src/napi.h", "src/napi-inl.h"
+    no_copy_source = True
+
+    def package(self):
+        self.copy("napi.h", src="src", dst="include")
+        self.copy("napi-inl.h", src="src", dst="include")
+
+    def package_id(self):
+        self.info.header_only()
+
