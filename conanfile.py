a27b4a45fb502324befe380bf92a622148f817fc
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-07-22 06:36:22 +0300

    Add missing header

--- conanfile.py
+++ conanfile.py
@@ -11,12 +11,13 @@ class CImgConan(ConanFile):
     license = "MIT https://raw.githubusercontent.com/nodejs/node-addon-api/master/LICENSE.md"
     description = "This module contains header-only C++ wrapper classes which simplify the use of the C based N-API provided by Node.js when using C++. It provides a C++ object model and exception handling semantics with low overhead."
     url = "https://github.com/odant/conan-node-addon-api"
-    exports_sources = "src/napi.h", "src/napi-inl.h"
+    exports_sources = "src/napi.h", "src/napi-inl.h", "src/napi-inl.deprecated.h"
     no_copy_source = True
 
     def package(self):
         self.copy("napi.h", src="src", dst="include")
         self.copy("napi-inl.h", src="src", dst="include")
+        self.copy("napi-inl.deprecated.h", src="src", dst="include")
 
     def package_id(self):
         self.info.header_only()
